80995850415b1e9e6b39870b46df6a36213cbc4c
Rafael <rafaelrbncp@gmail.com>
2017-10-30 14:45:51 -0200

    ajuste

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
         <div className="App-intro">
           <br />
           <Single />
